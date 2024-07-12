e2e37a60c337427d756a9a05687bcca2153d4320
Fabricio Ruiz Ponce <ponce.1714.frp@gmail.com>
2024-07-12 16:31:31 -0500

    Creacion del componente del formulario para registrar empleado

---
+++ src/app/components/employee-form/employee-form.component.ts
@@ -0,0 +1,38 @@
+// src/app/components/employee-form/employee-form.component.ts
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { Employee } from '../../models/Employee.interface';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee-form',
+  templateUrl: './employee-form.component.html',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule]
+})
+export class EmployeeFormComponent {
+  employeeForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private employeeService: EmployeeService) {
+    this.employeeForm = this.fb.group({
+      name: ['', Validators.required],
+      hourlyWage: [null, [Validators.required, Validators.min(0)]],
+      hoursWorked: [null, [Validators.required, Validators.min(0)]],
+      overtimeHours: [null, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.employeeForm.valid) {
+      const newEmployee: Employee = this.employeeForm.value;
+      this.employeeService.addEmployee(newEmployee);
+      this.employeeForm.reset();
+    }
+  }
+
+  controlHasError(controlName: string, errorName: string): boolean {
+    return this.employeeForm.controls[controlName].hasError(errorName) && this.employeeForm.controls[controlName].touched;
+  }
+}
