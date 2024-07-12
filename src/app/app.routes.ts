595358c64f7730ef1e204d3c3222695a77e6205e
Fabricio Ruiz Ponce <ponce.1714.frp@gmail.com>
2024-07-12 16:32:16 -0500

    Actualizacion del app.componente.* y el app.route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: EmployeeFormComponent },
+  // otras rutas si es necesario
+];
